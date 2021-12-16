a657d324fa8c627381a0f32255eab28d41a9da0f
ut <ut@users.noreply.github.com>
2021-12-16 19:14:32 +0000

    deploy: e1305087b6f9a795eed1f75205b8f257af9316a4

--- _nuxt/static/1639667161/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffgtq-client\u002F_nuxt\u002Fstatic\u002F1639667161",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ffgtq-client\u002F",assetsPath:"\u002Ffgtq-client\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bd5cfd3f"}}}}(null));
