670aef6cf92adc5dccd6943361d62fd543d94c42
Ulf Treger <ulf.treger@3plusx.de>
2022-01-25 20:02:05 +0100

    Improve hash navigation

--- components/Main.vue
+++ components/Main.vue
@@ -719,10 +719,13 @@ export default {
     },
     jumpToMap() {
       console.log("jumpToMap " + this.$route.hash )
+
       if ( ( this.$route.hash === '#list' ) ||  ( this.$route.hash === '#info' ) )  {
         this.$router.push({ name: 'main', hash: this.$route.hash })
+        location.hash = this.$route.hash;
       } else {
         this.$router.push({ name: 'main', hash: '#map' })
+        location.hash = '#map';
       }
     },
     scrollX(e) {
