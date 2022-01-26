b46c1b0b9acea3a9f43b2e4cf1b21bcc51db3a09
ut <ut@users.noreply.github.com>
2022-01-26 17:41:53 +0000

    deploy: 759fedd468ba935961a9566c4aafeabaf730ff8a

--- _nuxt/static/1643206249/main/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffgtq-client\u002F_nuxt\u002Fstatic\u002F1643206249",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmain",config:{_app:{basePath:"\u002Ffgtq-client\u002F",assetsPath:"\u002Ffgtq-client\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d2577a8c"}}}}(null));
---
+++ _nuxt/static/1643218912/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffgtq-client\u002F_nuxt\u002Fstatic\u002F1643218912",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ffgtq-client\u002F",assetsPath:"\u002Ffgtq-client\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bc14a013"}}}}(null));
