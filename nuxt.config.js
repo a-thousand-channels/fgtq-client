7bf58df5504f9b565c79183586096e23daa6d635
Ulf Treger <ulf.treger@3plusx.de>
2022-01-28 21:01:50 +0100

    Remove content plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,8 +71,6 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
-    // https://go.nuxtjs.dev/content
-    '@nuxt/content',
     ['nuxt-leaflet', { /* module options */ }],
   ],
 
@@ -98,10 +96,6 @@ export default {
       useWebmanifestExtension: false
     }
   },
-
-  // Content module configuration: https://go.nuxtjs.dev/config-content
-  content: {},
-
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   },
