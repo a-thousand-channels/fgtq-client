97d62e8cc2d9f8f6e5f9ac83e021ca5890df1a11
ut <ut@users.noreply.github.com>
2022-01-29 20:19:27 +0000

    deploy: 75f9e64c9730cdd79927c2d6e083bf33dd6f6a39

--- _nuxt/static/1643474765/layer/manu/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffgtq-client\u002F_nuxt\u002Fstatic\u002F1643474765",layout:"default",error:a,serverRendered:true,routePath:"\u002Flayer\u002Fmanu",config:{_app:{basePath:"\u002Ffgtq-client\u002F",assetsPath:"\u002Ffgtq-client\u002F_nuxt\u002F",cdnURL:a}}}}(null));
