a23cf39a840d54a8f570ca49a01fee4f89273a95
Ulf Treger <ulf.treger@3plusx.de>
2022-02-05 19:55:34 +0100

    Fix typo

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ export default {
       { rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-touch-icon.png' },
       { rel: 'icon', type: 'image/png', sizes: "32x32", href: '/favicon-32x32.png' },
       { rel: 'icon', type: 'image/png', sizes: "16x16", href: '/favicon-16x16.png' },
-      { rel: 'manifest', href: '/site.webmanifest' }
+      { rel: 'manifest', href: '/site.webmanifest' },
       { rel: 'mask-icon', href: '/safari-pinned-tab.sv', color: "#5bbad5" }
     ]
   },
