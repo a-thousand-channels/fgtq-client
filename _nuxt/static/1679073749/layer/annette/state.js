d8de997361cf03ed417d5ed3b0641acaf668265d
ut <ut@users.noreply.github.com>
2023-03-17 17:22:31 +0000

    deploy: b7ab29aa1720df90884e5a59cc42f8c50e85343a

---
+++ _nuxt/static/1679073749/layer/annette/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffgtq-client\u002F_nuxt\u002Fstatic\u002F1679073749",layout:"default",error:a,serverRendered:true,routePath:"\u002Flayer\u002Fannette",config:{_app:{basePath:"\u002Ffgtq-client\u002F",assetsPath:"\u002Ffgtq-client\u002F_nuxt\u002F",cdnURL:a}}}}(null));
