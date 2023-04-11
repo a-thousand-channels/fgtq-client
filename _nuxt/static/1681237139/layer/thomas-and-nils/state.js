51bcde0c3b4effb0a1eeff9a86ed32c108db8bff
ut <ut@users.noreply.github.com>
2023-04-11 18:26:37 +0000

    deploy: 81c734b163d9fc16291af6e9008826590167fce1

--- _nuxt/static/1681237139/layer/thomas-and-nils/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffgtq-client\u002F_nuxt\u002Fstatic\u002F1681237139",layout:"default",error:a,serverRendered:true,routePath:"\u002Flayer\u002Fthomas-and-nils",config:{_app:{basePath:"\u002Ffgtq-client\u002F",assetsPath:"\u002Ffgtq-client\u002F_nuxt\u002F",cdnURL:a}}}}(null));
